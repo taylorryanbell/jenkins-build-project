171b8d1e5538e408db6241385df1bd0ba60fe3cb
Adam Street <adam.street.dev@gmail.com>
2021-12-29 23:04:57 -0600

    created html generator

--- app.py
+++ app.py
@@ -1,29 +1,33 @@
-from flask import Flask
+import sys
+import numpy
 
-app = Flask(__name__)
+if len(sys.argv) != 2:
+    print("no file path")
+    exit(1)
 
-@app.route("/")
-def hello_world():
-    return """
-    <!DOCTYPE html>
-    <html lang="en">
-    
-    <head>
-      <meta charset="UTF-8">
-      <meta http-equiv="X-UA-Compatible" content="IE=edge">
-      <meta name="viewport" content="width=device-width, initial-scale=1.0">
-      <title>Document</title>
-    
-      <link rel="preconnect" href="https://fonts.googleapis.com">
-      <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-      <link href="https://fonts.googleapis.com/css2?family=Lobster+Two&display=swap" rel="stylesheet">
-    </head>
-    
-    <body style="overflow: hidden; background-color: ghostwhite;">
-      <div style="height: 100vh; width: 100vw; display: flex; justify-content: center; align-items: center">
-        <div style="font-family: 'Lobster Two'; font-size: 15rem;">Hello from Flask!</div>
-      </div>
-    </body>
-    
-    </html>
-    """
+file_path = sys.argv[1]
+file = open(file_path, "x")
+file.write(f"""
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Lobster+Two&display=swap" rel="stylesheet">
+</head>
+
+<body style="overflow: hidden; background-color: ghostwhite;">
+  <div style="height: 100vh; width: 100vw; display: flex; justify-content: center; align-items: center">
+    <div style="font-family: 'Lobster Two'; font-size: 15rem;">PI: {numpy.pi}</div>
+  </div>
+</body>
+
+</html>
+""")
+file.close()
